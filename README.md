73fc3bc14d69c7412de3462796262ed57c13a7dc
panuvit <jonh2544za@gmail.com>
2023-03-23 09:40:55 +0700

    make docker-compose for run web api and database

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Numerical-method
+
 website with react
-include fontend and backend
+include fontend, backend and mysql database.
+this website is implement numerical method.
+
+type "docker-compose up" in your terminal and then magic will happen!!
--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
 const express = require("express");
-const app = express()
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: 'host.docker.internal',
-    port: 3307,
-    user: 'root',
-    password: '1234',
-    database: 'numerical',
-});
+const app = express();
 
-connection.connect((error) => {
-    if (error) throw error;
-    console.log('Connected to MySQL server');
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     user: 'root',
+//     password: "pech2544za",
+//     database: 'numerical',
+// });
+
+// connection.connect((error) => {
+//     if (error) throw error;
+//     console.log('Connected to MySQL server');
+// });
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 app.get("/", (req, res) => {
@@ -22,13 +31,28 @@ app.get("/", (req, res) => {
 
 app.get("/sample/:method/:number", (req, res) => {
     res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
-    connection.query(`SELECT info FROM sample WHERE method = \"${req.params.method}\" and sample = ${req.params.number}`, (err, results) => {
-        if (err) {
-          throw err;
-        }
-        res.json(results);
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+    
+        const sql = `SELECT info FROM sample WHERE method = \"${req.params.method}\" and sample = ${req.params.number}`;
+
+        connection.query(sql, (err, result) => {
+          connection.release(); // release the connection back to the pool
+    
+          if (err) throw err;
+          res.json(result);
+
+        });
     });
+    // connection.query(`SELECT info FROM sample WHERE method = \"${req.params.method}\" and sample = ${req.params.number}`, (err, results) => {
+    //     if (err) {
+    //       throw err;
+    //     }
+    //     res.json(results);
+    // });
 });
 
 const port = process.env.PORT || 3001;
-app.listen(port);
+app.listen(port, () => {
+    console.log("server start port at " + port);
+});
