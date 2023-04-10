c4fbfde8da6239d0930a054681a8b42c1e4f061e
panuvit <jonh2544za@gmail.com>
2023-04-10 10:31:55 +0700

    dfg

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const app = require('./app.js')
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log("server start port at " + port);
+    console.log("server starts port at " + port);
 });
 
 module.exports = app;
